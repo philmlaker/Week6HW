be0604d76d0bc400ab7e5ae88f678575aa4469a2
Philip Mlaker <philipmlaker@Philips-MBP.attlocal.net>
2016-12-02 14:42:58 -0600

    Friday 2:42pm

--- add.js
+++ add.js
@@ -1,60 +1,65 @@
-$("document").ready(function() {  
-
-	$("input").on("keydown",function search(e) {
-    if(e.keyCode == 13) {
-    	var newAnimal = $(this).val();
-    	alert(newAnimal);
-
-    }
-});
-
-//THIS KEYPRESS WILL TRIGGER OUR AJAX CALL
-
-	
-    	var newAnimal = $("#animal-input").val();
-    	
-    	
- 
+$("document").ready(function() {
 
     
 
 
-function createBtns (x){
-	var animal = ["bird", "cat", x];
-	
+    $("input").on("keydown", function search(e) {
+        if (e.keyCode == 13) {
+            
+            
+
+            var newAnimal = $(this).val();
+
+         	var animalBtn = $("<button>");
 
 
-	for (var i = 0; i < animal.length; i++) {
+            animalBtn.text(newAnimal);
 
-		animal;
+            $("#animalButtons").after(animalBtn);
 
-		
-	    var animalBtn = $("<button>");
-
-	    animalBtn.attr("data-animal", animal[i]);
-
-	    animalBtn.text(animal[i]);
-
-	    $("#animalButtons").after(animalBtn);
-
-
-
-	    
-		
-}
-
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + [i] + "&api_key=dc6zaTOxFJmzC";
-
-
-$.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
-	        console.log(response);
-
-	    });
+        }
+    });
 
 
 
+createBtns();
+
+    function createBtns() {
+
+    		animal = ["dog", "cat"]
+ 
+        for (var i = 0; i < animal.length; i++) {
+
+ 
+            var animalBtn = $("<button>");
+
+            animalBtn.attr("data-animal", animal[i]);
+
+            animalBtn.text(animal[i]);
+
+            $("#animalButtons").after(animalBtn);
 
 
 
-	}
+
+
+        }
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + [i] + "&api_key=dc6zaTOxFJmzC";
+
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).done(function(response) {
+            console.log(response);
+
+        });
+
+
+
+
+
+
+    }
 });
