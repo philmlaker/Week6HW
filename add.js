e70222728e604e95d7f015679957b5efc8704596
Philip Mlaker <philipmlaker@Philips-MBP.attlocal.net>
2016-12-02 18:01:52 -0600

    Friday 6:01PM

--- add.js
+++ add.js
@@ -1,55 +1,93 @@
 $("document").ready(function() {
 
 
+    window.animalArray = ["cats", "dogs", "snakes"];
+
+    $("#submit").click(function() {
 
 
+        var newAnimalName = $("#animal-input").val();
+        // var newAnimalBtn = $('<input type="button"/>');
+        // $(newAnimalBtn).val(newAnimalName);
+        // newAnimalBtn.appendTo($("body"));
+        $("form").trigger("reset");
+        animalArray.push(newAnimalName);
+        createBtns();
 
-    $("body").on("keypress", function search(e) {
-        if (e.keyCode == 13) {
-
-            var newAnimalName = $(this).val();
-            var newAnimalBtn = $('<input type="button"/>');
-            $(newAnimalBtn).val("newAnimalName");
-            newAnimalBtn.appendTo($("body"));
-
-              
-        }
     });
 
 
-   
 
     function createBtns() {
 
-        animal = ["dog", "cat"]
 
-        for (var i = 0; i < animal.length; i++) {
 
+        animalArray;
+        $("#buttonContain").html("");
+
+        for (var i = 0; i < animalArray.length; i++) {
+
+            var animalSearch = animalArray[i];
 
             var animalBtn = $("<button>");
 
-            animalBtn.attr("data-animal", animal[i]);
+            animalBtn.attr("data-animal", animalArray[i]);
 
-            animalBtn.text(animal[i]);
+            animalBtn.text(animalArray[i]);
 
-            $("#animalButtons").after(animalBtn);
+            $("#buttonContain").append(animalBtn);
+
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalSearch + "&api_key=dc6zaTOxFJmzC";
+
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            }).done(function(response) {
+
+                console.log(response);
+
+                var results = response.data;
+
+                var gifDiv = $("<div class='item'> <div id='imgAnimate'>");
+
+                var animalImage = $("<img>");
+
+                animalImage.attr("src", results[i].images.fixed_height.url);
+                
+
+                gifDiv.append(animalImage);
+
+                $("#gifs-appear-here").prepend(gifDiv);
+
+
+            });
+
+
+
+                $("#imgAnimate").hover(
+                    function() {
+                        $(this).attr("src", results[i].images.fixed_height.url);
+                    },
+                    function() {
+                        $(this).attr("src", results[i].images.fixed_height_still.url);
+                    });
 
 
 
 
 
-        }
-
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + [i] + "&api_key=dc6zaTOxFJmzC";
 
 
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).done(function(response) {
-            console.log(response);
 
-        });
+
+
+
+
+
+        };
+
+
+
 
 
 
